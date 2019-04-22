b8caec4586b644361e5a8835585a3120a1204133
Jen Luker <jluker@deseretdigital.com>
2019-04-22 11:22:27 -0600

    [WIP]
    Learning hooks, and setting up framework

--- src/index.js
+++ src/index.js
@@ -1,18 +1,87 @@
 /* @jsx jsx */
 import { jsx } from '@emotion/core'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import Result from './components/Result'
 
 import './styles.css'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App" css={{ color: 'darkgray' }}>
-        <input type="text" placeholder="Search NPM" />
-      </div>
-    )
+function getPackages(str) {
+  const [results, setResults] = useState([])
+
+  async function fetchPackages(str) {
+    const API = `https://api.npms.io/v2/search/suggestions?q=${str}`
+    const res = await fetch(API, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+
+    const data = await res.json()
+    setResults(data)
   }
+
+  useEffect(() => {
+    fetchPackages()
+  }, [str])
+
+  return [results, fetchPackages]
+}
+
+const App = () => {
+  const [search, setSearch] = useState('')
+  const [packages, fetchPackages] = getPackages(search)
+
+  return (
+    <div className="App" css={{ color: 'darkgray' }}>
+      <header>
+        <form onSubmit={() => fetchPackages(search)}>
+          <input
+            type="text"
+            placeholder="Search NPM"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+
+        <nav>
+          <div># Packages Found</div>
+          <div>Pagination</div>
+        </nav>
+      </header>
+      <main>
+        <aside>
+          <div className="sidebar">
+            <div>Sort Packages</div>
+            <div className="optimal">Optimal</div>
+            <div className="popularity">Popularity</div>
+            <div className="quality">Quality</div>
+            <div className="maintenance">Maintenance</div>
+          </div>
+        </aside>
+        <section className="content">
+          <p>Search Results</p>
+          {packages.length ? (
+            packages.map((result, idx) => {
+              const key =
+                result.package.name ||
+                `${result.package.links.npm}_${idx}`
+              return (
+                <Result
+                  data={result.package}
+                  score={result.score}
+                  key={key}
+                />
+              )
+            })
+          ) : (
+            <article>No results found</article>
+          )}
+        </section>
+      </main>
+    </div>
+  )
 }
 
 const rootElement = document.getElementById('root')
